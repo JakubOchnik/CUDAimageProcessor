b73ccff6cff124e3cd589d09412c9958b48ec1ad
JakubOchnik <jakub.ochnik1@gmail.com>
2021-06-27 11:45:06 +0200

    Fix different parameter names in declaration and definition (loadImg)

--- ImgHandling/ImgLoader.h
+++ ImgHandling/ImgLoader.h
@@ -11,8 +11,8 @@ public:
 	// checks if image dimensions are within the limit
 	static bool checkImgDims(int x, int y);
 	// multiple overloaded instances for better flexibility:
-	static int loadImg(std::string path, int type, Img& src);
-	static int loadImg(std::string path, Img& src);
+	static int loadImg(std::string path, int type, Img& dst);
+	static int loadImg(std::string path, Img& dst);
 	static Img loadImg(std::string path, int type);
 	static Img loadImg(std::string path);
 };
